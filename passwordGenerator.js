77fc72398aa8762d5d1f7c3fd68a1decc1e72993
SirPotatoIV <otoolej33@gmail.com>
2019-11-13 15:26:46 -0600

    Added link to js code in html file. Created first attempt at psuedo code in js file.

---
+++ passwordGenerator.js
@@ -0,0 +1,11 @@
+// Define all possible characters that can be used in the password. Separate the characters in arrays by option types the user will get.
+// Define a const for each character type that will store the users criteria
+// Get users password criteria and store it in the consts
+    // Validate that the user has selected at least one character type.
+// Create a let that will be a random index selector to select one of the character arrays.
+// Create a let that will be a random selector for a character in the randomly selected array.
+// Create a let that will store the set of randomly selected characters that will amount to a password.
+// Select characters from arrays and combine them into a password
+    // Use a loop to continue selecting characters from different arrays at random and updating the let password.
+    // The loop will end when when the length the user selected (should be stored in a const) is reached.
+// Updated the html of a box (probably a jumbotron) so that the text in said box is the password
