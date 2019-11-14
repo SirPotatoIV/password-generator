04a577cf5c1d50c36c4026d5ed0c34582a61ecdc
SirPotatoIV <otoolej33@gmail.com>
2019-11-13 19:09:30 -0600

    Added an array that will store all the input types the user has selected, currently just set to specialCharacters for testing purposes. Created const sc to check if special characters was a type of character selected by the user. Added an if statement in the for loop so that if the user selected special characters then special characters would be added to the password. sc is used to decide whether the loop should enter this if statement.

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,5 +1,7 @@
 // Define all possible characters that can be used in the password. Separate the characters in arrays by option types the user will get.
 // APPEARS THAT THIS STRATEGY WON'T WORK const characterArrays = ["specialCharacters", "numCharacters",];
+// Used to store the names of the character types the user has selected. This will be used to figure out what characters characterSelector should use.
+const selectedCharTypes = ["specialCharacters",];
 const specialCharacters = ["!", "#", "$", "+", "?", "@", "%",];
 const numCharacters = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9",];
 // Define a const for each character type that will store the users criteria
@@ -21,17 +23,22 @@ console.log(arrayLength, characterIndex);
     let userPassword = "";
     let randomChar = specialCharacters[characterIndex];
     console.log(randomChar);
+    // Used to see if specialCharacters is a character type picked by the user. Used in the function characterSelector.
+    sc = selectedCharTypes.indexOf("specialCharacters");
+    console.log(sc);
     
     function characterSelector() {
         // The purpose of this loop is to combine the user required number of characters into userPassword.
         for(i = 0; i < passwordLength; i++) {
             // The purpose of this loop is to ensure that one character from each selected character type is selected.
             for(i = i; i < 4; i++) {
-                userPassword = userPassword + randomChar;
-                // console.log(userPassword, "Loop 1");
+                if(sc > -1){
+                    userPassword = userPassword + randomChar;
+                    console.log(userPassword, "Loop 1");
+                };
             }
                 userPassword = userPassword + randomChar;
-                // console.log(userPassword, "Loop 2");
+                console.log(userPassword, "Loop 2");
         }
     }
 
