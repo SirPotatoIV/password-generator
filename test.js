c10d2e0ab22fc47dcac9ca5d4beb7ce3403f3b30
SirPotatoIV <otoolej33@gmail.com>
2019-11-14 16:12:25 -0600

    removed bootstrap classes from the input. Unsure if that was truly the root of my issues or the website I was reading to change the possible range of the range selector was incorrect. Switched to the mdn documentation and was able to find the correct attributes to add to range selector element.

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ sliderEl = document.getElementById('formControlRange');
 console.log(sliderEl)
 sliderDisplayEl = document.getElementById('sliderDisplay');
 console.log(sliderDisplayEl);
-// passwordLength = sliderEl.value;
-passwordLength = "9"
-console.log(passwordLength);
+passwordLength = sliderEl.value;
+// passwordLength = "9"
+// console.log(passwordLength);
+// https://www.java67.com/2012/10/best-way-to-convert-numbers-to-string-in-java-example.html
 passwordString = passwordLength.valueOf(); 
 
 function changeSlider() {
