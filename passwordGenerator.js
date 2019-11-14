270c5d9643bd4c9ab900010a7af7a8edd09534de
SirPotatoIV <otoolej33@gmail.com>
2019-11-13 18:21:47 -0600

    Started work adding characters to use in the password. Created an array called specialCharacters, which contians the special characters I want to use in the generated password. Nothing much required to test this. Created a way to generate a random index to select a character at random in a character array. Currently it is just set up to find the length of specialCharacter array for testing purposes. Used console.log to make sure the length calculation is correct and the character index is a number between 0 and the length. Changed randomChar to be equal to a character from the specialCharacters array picked with the randomChar. Checked what random character was picked with a console log. Loop still working.

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,20 +1,26 @@
 // Define all possible characters that can be used in the password. Separate the characters in arrays by option types the user will get.
+const specialCharacters = ["!", "#", "$", "+", "?", "@", "%",];
 // Define a const for each character type that will store the users criteria
 // Get users password criteria and store it in the consts
     // Validate that the user has selected at least one character type.
 // Create a let that will be a random index selector to select one of the character arrays.
 // Create a let that will be a random selector for a character in the randomly selected array.
+const characterIndex = Math.floor(Math.random()*specialCharacters.length);
+console.log(specialCharacters.length, characterIndex);
+
 // Create a let that will store the set of randomly selected characters that will amount to a password.
 // Select characters from arrays and combine them into a password
     // Use a loop to continue selecting characters from different arrays at random and updating the let password.
     // The loop will end when when the length the user selected (should be stored in a const) is reached.
     const passwordLength = 8;
     let userPassword = "";
-    let randomChar = "a";
+    let randomChar = specialCharacters[characterIndex];
+    console.log(randomChar);
     
     function characterSelector() {
-        
+        // The purpose of this loop is to combine the user required number of characters into userPassword.
         for(i = 0; i < passwordLength; i++) {
+            // The purpose of this loop is to ensure that one character from each selected character type is selected.
             for(i = i; i < 4; i++) {
                 userPassword = userPassword + randomChar;
                 console.log(userPassword, "Loop 1");
